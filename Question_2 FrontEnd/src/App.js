8f872d8588fb86c31553cf409727360fe94550ab
prashantcoding <soultaker399@gmail.com>
2023-08-20 12:55:00 +0530

    completed data showing

--- Question_2 FrontEnd/src/App.js
+++ Question_2 FrontEnd/src/App.js
@@ -6,7 +6,7 @@ import '../src/Component/component.css'
 const App = () => {
   const [trainData,setTrainDdata]=useState(null);
   useEffect(() => {
-    
+    const currentTime = new Date();
     async function getTrain(){
        const response= await axios({
         method: "get",
@@ -14,9 +14,20 @@ const App = () => {
         
       
     })
+   
+      
     
     if(response){
-      setTrainDdata(response.data);
+      const thirtyMinutesFromNow = new Date(currentTime.getTime() + 30 * 60 * 1000);
+      const filteredTrains = response.data.filter(train => {
+        const departureTime = new Date();
+        departureTime.setHours(train.departureTime.Hours);
+        departureTime.setMinutes(train.departureTime.Minutes);
+        departureTime.setSeconds(train.departureTime.Seconds);
+      
+        return   departureTime >= thirtyMinutesFromNow;
+      });
+      setTrainDdata(filteredTrains);
     }
     
     
@@ -38,10 +49,13 @@ const App = () => {
               <th>Train Number</th>
               <th>SeatAvalaible</th>
               <th>departTureTime</th>
+              <th>price</th>
+              <th>delayedBy</th>
             </tr>
           </thead>
           <tbody>
           {  trainData && trainData.map((train)=>(<SingleTrain key={train.trainNumber} train={train}/>))}
+
           </tbody>
         </table>
       </div>
--- Question_2 FrontEnd/src/Component/SingleTrain.js
+++ Question_2 FrontEnd/src/Component/SingleTrain.js
@@ -7,7 +7,22 @@ const SingleTrain = ({train}) => {
     <tr>
       <td>{train.trainName}</td>
       <td>{train.trainNumber}</td>
-      
+      <td>
+        <td>AC {train.seatsAvailable.AC}</td>
+        <td>sleeper {train.seatsAvailable.sleeper}</td>
+        <td> {train.seatsAvailable.sleeper}</td>
+       
+      </td>
+      <td> {train.departureTime.Hours+":"+train.departureTime.Minutes+":"+train.departureTime.Seconds}</td>
+      <td>
+        <td>AC {"Rs."+train.price.AC} </td>
+        <td>sleeper {"Rs."+train.price.sleeper}</td>
+       
+       
+      </td>
+      <td>{
+        <td >{train.delayedBy +"minutes"}</td>
+        }</td>
     </tr>
     
  
