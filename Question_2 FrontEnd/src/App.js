5817669b201310640593285e9ad4e3c35601d02f
prashantcoding <soultaker399@gmail.com>
2023-08-20 11:17:03 +0530

    setup base backend

--- Question_2 FrontEnd/src/App.js
+++ Question_2 FrontEnd/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import NavBar from './Component/NavBar'
 
 const App = () => {
   return (
     <div>
-      
+      <NavBar/>
+      <h1>Train Schedules </h1>
     </div>
   )
 }
---
+++ Question_2 FrontEnd/src/Component/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './component.css'
+const NavBar = () => {
+  return (
+    <div className='NavContainer'>
+      
+    </div>
+  )
+}
+
+export default NavBar
