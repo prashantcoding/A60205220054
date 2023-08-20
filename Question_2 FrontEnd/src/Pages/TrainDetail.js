905a3c039ccb48ccb0a0d686c9f027dea8c7811c
prashantcoding <soultaker399@gmail.com>
2023-08-20 13:52:24 +0530

    added ScreenShots

--- Question_2 FrontEnd/src/Pages/TrainDetail.js
+++ Question_2 FrontEnd/src/Pages/TrainDetail.js
@@ -3,42 +3,40 @@ import SingleTrain from '../Component/SingleTrain'
 import axios from 'axios' 
 import '../Component/component.css'
 const TrainDetail = () => {
-  const [trainData,setTrainData]=useState(null);
+  const [trainData,setTrainDdata]=useState(null);
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      const currentTime = new Date();
+    const currentTime = new Date();
+    async function getTrain(){
+       const response= await axios({
+        method: "get",
+        url: "http://localhost:5000/train/train",
+        
       
-      async function getTrain() {
-        try {
-          const response = await axios.get("http://localhost:5000/train/train");
-
-          if (response.data) {
-            const thirtyMinutesFromNow = new Date(currentTime.getTime() + 30 * 60 * 1000);
-            const filteredTrains = response.data.filter(train => {
-              const departureTime = new Date();
-              departureTime.setHours(train.departureTime.Hours);
-              departureTime.setMinutes(train.departureTime.Minutes);
-              departureTime.setSeconds(train.departureTime.Seconds);
-
-              return departureTime >= thirtyMinutesFromNow;
-            });
-            
-            setTrainData(filteredTrains);
-            localStorage.setItem("trainData", JSON.stringify(filteredTrains));
-          }
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      }
-
-      getTrain();
-    }, 4000);
-
-    // Clean up the interval when the component unmounts
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [])
+    })
+   
+      
+    
+    if(response){
+      const thirtyMinutesFromNow = new Date(currentTime.getTime() + 30 * 60 * 1000);
+      const filteredTrains = response.data.filter(train => {
+        const departureTime = new Date();
+        departureTime.setHours(train.departureTime.Hours);
+        departureTime.setMinutes(train.departureTime.Minutes);
+        departureTime.setSeconds(train.departureTime.Seconds);
+      
+        return   departureTime >= thirtyMinutesFromNow;
+      });
+      setTrainDdata(filteredTrains);
+      localStorage.setItem("trainData",JSON.stringify(filteredTrains));
+    }
+    
+    
+    
+  }
+  getTrain();
+  
+ 
+  },[])
   return (
     <div>
       <div >
